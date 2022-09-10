11e19668010742261328bcef19062cc82859aa39
Evie Dickman <GeneralEvie9@outlook.com>
2022-09-10 12:47:23 -0500

    modal working

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
-$(document).ready(() =>
-{
-    
-    $('.month').click(function()
-    {
-        $(event.currentTarget).next('.calendar').slideToggle();
+const openCalendar = document.querySelectorAll('h1')
+const Calendar = document.getElementById('calendar')
+const Overlay = document.getElementById('calendar-overlay')
+const Cancel = document.querySelectorAll('.cancel')
 
-    });
-});
+openCalendar.forEach(elem => elem.addEventListener('click', active))
+Cancel.forEach(elem => elem.addEventListener('click', close))
+
+Overlay.addEventListener('click', () => {
+    const Menus = document.querySelectorAll('#calendar-overlay.active')
+    Menus.forEach(elem => {
+        close(elem)
+    })
+})
+
+function active()
+{
+    Calendar.classList.add('active')
+    Overlay.classList.add('active');
+}
+
+function close()
+{
+    Calendar.classList.remove('active')
+    Overlay.classList.remove('active');
+}
